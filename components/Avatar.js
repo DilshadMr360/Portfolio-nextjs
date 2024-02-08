b00677d1a22b240a17b4f20dcd2e243f3a89c992
DilshadMr360 <mohomeddilshad123@gmail.com>
2024-02-08 06:59:17 +0530

    image changes

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
-    <Image src={'/mypic5.jpg'} width={650} height={450} alt="" className="translate-z-0 w-[400px] h-[600px]"/>
+    <Image src={'/mypic4.png'} width={650} height={450} alt="" className="translate-z-0 w-[400px] h-[600px]"/>
   </div>;
 };
 
