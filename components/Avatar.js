59cec26112040c5630177f4e4748c6a8f9011bec
DilshadMr360 <mohomeddilshad123@gmail.com>
2024-07-09 13:58:08 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Avatar = () => {
   return (
     <div className="hidden h-full xl:flex xl:items-center xl:justify-center xl:max-w-none">
-      <Image src={'/Dil1.JPG'} width={440} height={200} alt="" className="translate-z-0" />
+      <Image src={'/Dil1.JPG'} width={400} height={200} alt="" className="translate-z-0" />
     </div>
   );
 
